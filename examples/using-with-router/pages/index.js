3fa2dca3f58cdb9c2b4bfce85a9452591dde103b
xyyjk <xyyjk@126.com>
2018-06-28 21:38:15 +0800

    IE9 bug

--- examples/using-with-router/pages/index.js
+++ examples/using-with-router/pages/index.js
@@ -1,7 +1,11 @@
+import Head from 'next/head'
 import Header from '../components/Header'
 
 export default () => (
   <div>
+    <Head>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/babel-polyfill/6.26.0/polyfill.min.js" />
+    </Head>
     <Header />
     <p>HOME PAGE is here!</p>
   </div>
